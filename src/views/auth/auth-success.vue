bb895ad103e9aa99917bd4f42bd7d1bb74ec2a68
Armaldio <armaldio@gmail.com>
2025-02-26 12:21:25 +0100

    add auth success

---
+++ src/views/auth/auth-success.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>You can now close this window</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { supabase } from '@/utils/supabase';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route  = useRoute()
+const isLoading = ref(true)
+
+console.log('route', route)
+
+// // Example webhook call - adjust payload as needed for signUp response
+// await supabase.functions.invoke('webhook-post-account-creation', {
+//   body: {
+//     id: data.user.id // Use data.user.id from signUp response
+//   }
+// })
+// user.value = data.user
+// setAuthState('SIGNED_IN')
+// fetchSubscription()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    Icons(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+export default defineConfig((env) => {
+  const environment = loadEnv(env.mode, process.cwd(), '')
+
+  return ({
+    plugins: [
+      vue(),
+      vueDevTools(),
+      Icons(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      __SUPABASE_URL__: JSON.stringify(environment.SUPABASE_URL),
+      __SUPABASE_ANON_KEY__: JSON.stringify(environment.SUPABASE_ANON_KEY),
+      __SUPABASE_PROJECT_ID__: JSON.stringify(environment.SUPABASE_PROJECT_ID),
+    },
+  })
 })
